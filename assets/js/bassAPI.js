b8d9c3b3e27a33a84b6259604c38ed03cc84ac1d
zqxtyl <2571756029@qq.com>
2022-06-09 20:05:03 +0800

    修改了首页

--- assets/js/bassAPI.js
+++ assets/js/bassAPI.js
@@ -1,3 +1,9 @@
 $.ajaxPrefilter((options) => {
   options.url = 'http://www.liulongbin.top:3007' + options.url;
+
+  if(options.url.includes('/my/')){
+    options.headers={
+      Authorization:localStorage.getItem('token')
+    }
+  }
 });
