e8bfce9a54bb5cc4b23b47cbfa14a5ad729d68c7
zqxtyl <2571756029@qq.com>
2022-06-09 16:16:27 +0800

    完成了注册和登录

--- assets/js/login.js
+++ assets/js/login.js
@@ -17,13 +17,46 @@ $(function () {
     repwd: (value) => {
       const pwd = $('.reg-box [name=password]').val();
 
-      console.log(pwd,value);
+      console.log(pwd, value);
 
       if (pwd !== value) return '两次密码不一样';
     },
   });
 
+  //   const baseUrl = 'http://www.liulongbin.top:3007';
+
   $('#form_reg').submit((e) => {
     e.preventDefault();
+
+    $.ajax({
+      type: 'POST',
+      url: '/api/reguser',
+      data: {
+        username: $('#form_reg [name=username]').val(),
+        password: $('#form_reg [name=password]').val(),
+      },
+      success: (res) => {
+        if (res.status !== 0) return layer.msg(res.message);
+        layer.msg('注册成功');
+        $('#link_login').click();
+      },
+    });
+  });
+
+  $('#form_login').submit(function (e) {
+    e.preventDefault();
+    console.log($(this).serialize());
+    $.ajax({
+      type: 'POST',
+      url: '/api/login',
+      data: $(this).serialize(),
+      success: (res) => {
+        if (res.status !== 0) return layer.msg('登录失败');
+        layer.msg('登录成功');
+
+        localStorage.setItem('token', res.token);
+        location.href = '/index.html';
+      },
+    });
   });
 });
